06098800a4474b789aaf03fbf2827281fcba5e8f
Noredeen Al-Zu'bi <noredeenalzubi@gmail.com>
2021-06-06 18:44:03 -0400

    start setting up in-memory storage

--- main.c
+++ main.c
@@ -4,6 +4,11 @@
 #include <string.h>
 #include <stdbool.h>
 
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+
 typedef struct {
     char *buffer;
     size_t buffer_length;
@@ -12,13 +17,40 @@ typedef struct {
 
 typedef enum { STATMENET_INSERT, STATEMENT_SELECT } StatementType;
 
-typedef enum { PREPARE_SUCC, PREPARE_UNRECOGNIZED_STMT } PrepareStatement;
+typedef enum { PREPARE_SUCC, PREPARE_UNRECOGNIZED_STMT, PREPARE_SYNTAX_ERROR } PrepareStatement;
+
+typedef struct {
+    uint32_t id;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE];
+} Row;
 
 typedef struct {
     StatementType type;
+    Row row; // Only needed for insert
 } Statement;
 
 typedef enum { META_CMD_SUCC, META_CMD_UNRECOGNIZED } MetaCmdResult;
+
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+
+// Does the row need to be a ptr??
+void serialize_row(Row *source, void *destination) {
+    memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+    memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+    memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+
+void deserialize_row(void *source, Row *destination) {
+    memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+    memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+    memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+}
 
 InputBuffer* new_input_buffer() {
     InputBuffer *buf = (InputBuffer*)malloc(sizeof(InputBuffer));
@@ -51,8 +83,14 @@ void read_input(InputBuffer *buf) {
 PrepareStatement prepare_stmt(InputBuffer *buf, Statement *stmt) {
     if (strncmp(buf->buffer, "insert", 6) == 0) {
         stmt->type = STATMENET_INSERT;
+        int args_assigned = sscanf(buf->buffer, "insert %d %s %s", &(stmt->row.id), stmt->row.username, stmt->row.email);
+
+        if (args_assigned < 3)
+            return PREPARE_SYNTAX_ERROR;
+  
         return PREPARE_SUCC;
     }
+
     if (strncmp(buf->buffer, "select", 6) == 0) {
         stmt->type = STATEMENT_SELECT;
         return PREPARE_SUCC;
